ff39e1ccc8ae703b830337b8a6f2df0e23631f1a
frank <frank@13e4b0b2-d838-11dd-a412-a1792178251d>
2007-08-21 21:01:49 +0000

    TSM-2 spike * refactored directory structure

---
+++ tsm/src/main/com/tech4d/tsm/model/Event.java
@@ -0,0 +1,77 @@
+package com.tech4d.tsm.model;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="EVENTS")
+public class Event {
+    private Long id;
+
+    private String title;
+    
+    @Temporal(TemporalType.TIME)
+    private Date date;
+
+    public Event() {}
+
+    @Id 
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    @SuppressWarnings("unused")
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    private Set<Person> participants = new HashSet<Person>();
+
+    @ManyToMany(
+            targetEntity=com.tech4d.tsm.model.Person.class,
+            cascade={CascadeType.PERSIST, CascadeType.MERGE}
+    )
+    @JoinTable(
+            name="PERSON_EVENT",
+            joinColumns={@JoinColumn(name="EVENT_ID")},
+            inverseJoinColumns={@JoinColumn(name="PERSON_ID")}
+    )
+    public Set<Person> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(Set<Person> participants) {
+        this.participants = participants;
+    }
+
+}
