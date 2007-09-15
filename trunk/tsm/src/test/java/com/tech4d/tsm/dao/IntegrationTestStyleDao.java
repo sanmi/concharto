91b1311b2c2e50320fe716748c4f53a350d450e4
frank <frank@13e4b0b2-d838-11dd-a412-a1792178251d>
2007-09-15 21:27:06 +0000

    TSM-5 Model * moved and renamed factory class * added style dao test * added fork="no" to integration tests

---
+++ trunk/tsm/src/test/java/com/tech4d/tsm/dao/IntegrationTestStyleDao.java
@@ -0,0 +1,63 @@
+package com.tech4d.tsm.dao;
+
+import com.tech4d.tsm.model.geometry.Style;
+import com.tech4d.tsm.util.ContextUtil;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+/**
+ * User: frank
+ * Date: Sep 15, 2007
+ * Time: 4:45:54 PM
+ */
+public class IntegrationTestStyleDao {
+
+    private static StyleDao styleDao;
+
+    @Before
+    public void setUp() {
+        ApplicationContext appCtx = ContextUtil.getCtx();
+        styleDao = (StyleDao) appCtx.getBean("styleDao");
+
+        //delete everything and save 1 style object
+        StyleUtil.setupStyle();
+    }
+
+    @Test
+    public void save() {
+        //save a second style
+        Style original = StyleUtil.getStyle();
+        Style style = styleDao.findById(StyleUtil.getStyle().getId());
+        assertEquals(original.getId(), style.getId());
+        assertEquals(original.getLineStyle().getWidth(), style.getLineStyle().getWidth());
+        assertEquals(original.getBaloonStyle().getBgColor(), style.getBaloonStyle().getBgColor());
+    }
+
+    @Test
+    public void delete() {
+        styleDao.delete(StyleUtil.getStyle());
+        Style style = styleDao.findById(StyleUtil.getStyle().getId());
+        assertNull(style);
+    }
+
+    public void deleteById() {
+        //test delete by
+        styleDao.delete(StyleUtil.getStyle().getId());
+        Style style = styleDao.findById(StyleUtil.getStyle().getId());
+        assertNull(style);
+    }
+
+    @Test
+    public void findAll() {
+        //save a second style
+        styleDao.save(new Style());
+        List<Style> styles = styleDao.findAll();
+        assertEquals(2, styles.size());
+    }
+
+}
