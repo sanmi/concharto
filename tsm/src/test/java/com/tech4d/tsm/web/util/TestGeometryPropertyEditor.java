ca4f8b5bca91801a1b3ef1d4e9cb49c00707599f
frank <frank@13e4b0b2-d838-11dd-a412-a1792178251d>
2007-10-19 18:46:26 +0000

    TSM-33 -  beta server - prepare and deploy * fixed linux incompatibilities: mysql table names are case sensitive in linux  and there was an ordering problem while testing JSON code

--- tsm/src/test/java/com/tech4d/tsm/web/util/TestGeometryPropertyEditor.java
+++ tsm/src/test/java/com/tech4d/tsm/web/util/TestGeometryPropertyEditor.java
@@ -2,6 +2,8 @@ package com.tech4d.tsm.web.util;
 
 import static org.junit.Assert.*;
 
+import net.sf.json.JSONObject;
+
 import org.junit.Test;
 
 import com.vividsolutions.jts.geom.Coordinate;
@@ -17,17 +19,23 @@ public class TestGeometryPropertyEditor {
     private GeometryPropertyEditor propertyEditor = new GeometryPropertyEditor();
 
     @Test
-    public void valueAsText() {
+	public void valueAsText() {
         propertyEditor.setAsText(POINT_TEXT);
         assertEquals(LAT, ((Point) (propertyEditor.getValue())).getY(), FP_DELTA);
         assertEquals(LNG, ((Point) (propertyEditor.getValue())).getX(), FP_DELTA);
-        assertEquals(POINT_TEXT, propertyEditor.getAsText());
+        verifyText(propertyEditor.getAsText());
+    }
+    
+    private void verifyText(String text) {
+        JSONObject point = JSONObject.fromObject(text);
+        assertEquals(LAT, point.get("lat"));
+        assertEquals(LNG, point.get("lng"));        
     }
 
     @Test public void value() {
         GeometryFactory gf = new GeometryFactory();
         Point point = gf.createPoint(new Coordinate(LNG, LAT));
         propertyEditor.setValue(point);
-        assertEquals(POINT_TEXT, propertyEditor.getAsText());
+        verifyText(propertyEditor.getAsText());
     }
 }
