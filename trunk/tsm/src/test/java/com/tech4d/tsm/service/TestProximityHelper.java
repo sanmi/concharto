dbcdbc7f6761da1f1425d6719e6ee25c7f6404ce
frank <frank@13e4b0b2-d838-11dd-a412-a1792178251d>
2007-11-07 18:52:54 +0000

    TSM-53 - web controller logging is way too verbose! * fixed unit test.  It works inside myeclipse but gets a compile error from the build file.  argh.

--- trunk/tsm/src/test/java/com/tech4d/tsm/service/TestProximityHelper.java
+++ trunk/tsm/src/test/java/com/tech4d/tsm/service/TestProximityHelper.java
@@ -20,25 +20,25 @@ public class TestProximityHelper {
 	@Test public void testGetBoundingBox() {
 		Set<Geometry> boxes = ProximityHelper.getBoundingBox(34.02/2, makePoint( -75.58672, 38.371528));
 		for (Geometry box : boxes) {
-			assertEquals(-75.9D, round(box.getCoordinate().x,1));
+			assertEquals(-75.9D, box.getCoordinate().x, .1);
 		}
 		boxes = ProximityHelper.getBoundingBox(22.29/2, makePoint( -75.58672, 38.371528));
         for (Geometry box : boxes) {
-            assertEquals(38.3D, round(box.getCoordinate().y,1));
+            assertEquals(38.3D, box.getCoordinate().y, .1);
         }
 	}
 	
 
 	@Test public void distance() {
-		assertEquals(41.10D, round(ProximityHelper.getDistance(
+		assertEquals(41.10D, ProximityHelper.getDistance(
 				makePoint(-75.901107, 38.205274), 
-				makePoint(-75.272140, 38.536887)), 2));
-		assertEquals(22.94D, round(ProximityHelper.getDistance(
+				makePoint(-75.272140, 38.536887)), .01);
+		assertEquals(22.94D, ProximityHelper.getDistance(
 				makePoint(-75.90110778808594, 38.53688722680627),
-				makePoint(-75.90110778808594, 38.205274034117814)), 2));
-		assertEquals(34.03D, round(ProximityHelper.getDistance(
+				makePoint(-75.90110778808594, 38.205274034117814)), .01);
+		assertEquals(34.03D, ProximityHelper.getDistance(
 				makePoint(-75.27214050292969, 38.53688722680627),
-				makePoint(-75.90110778808594, 38.53688722680627)),2));
+				makePoint(-75.90110778808594, 38.53688722680627)), .01);
 	}
 	
 	private Point makePoint(double x, double y) {
