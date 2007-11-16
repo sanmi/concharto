ecaf2455195e7c30eec2347b3c844c49e5b99fb9
frank <frank@13e4b0b2-d838-11dd-a412-a1792178251d>
2007-11-16 00:43:51 +0000

    TSM-38 - home page *added sensible map defaults

---
+++ trunk/tsm/src/main/java/com/tech4d/tsm/util/SensibleMapDefaults.java
@@ -0,0 +1,31 @@
+package com.tech4d.tsm.util;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+
+public class SensibleMapDefaults {
+    public static int ACCURACY_TO_ZOOM[] = {4, 5, 7, 9, 10, 12, 13, 14, 15};
+    public static final double SEARCH_BOX_MIN = 40D; //approximate bounding box = miles * 1.4
+    public static final double SEARCH_BOX_MAX = 2000D; //approximate bounding box = miles * 1.4
+    public static final int ZOOM_BOX_THRESHOLD = 10;
+    public static final int ZOOM_COUNTRY = 5;
+    public static final int ZOOM_USA = 4;
+    public static int NUM_ZOOM_LEVELS = 19;
+    public static double[] SEARCH_BOX_DIMENTSIONS = new double[NUM_ZOOM_LEVELS];
+    public static Point USA;
+    
+    static {
+    	//the low zoom levels have variable search boxes
+    	int i=0;
+    	for ( ; i<ZOOM_BOX_THRESHOLD; i++) { 
+    		SEARCH_BOX_DIMENTSIONS[i] = SEARCH_BOX_MAX - i*(SEARCH_BOX_MAX/ZOOM_BOX_THRESHOLD) + SEARCH_BOX_MIN;
+    	}
+    	//anything over threshold has the same search box 
+    	for ( ; i<NUM_ZOOM_LEVELS; i++) {
+    		SEARCH_BOX_DIMENTSIONS[i] = SEARCH_BOX_MIN;
+    	}
+    	GeometryFactory gf = new GeometryFactory();
+    	USA = gf.createPoint(new Coordinate(-96.667916, 37.013086));
+    }
+}
