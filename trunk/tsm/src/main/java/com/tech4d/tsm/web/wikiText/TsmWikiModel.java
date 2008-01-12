b96658caff7b787afda2e690cc7b4226d1e009b0
frank <frank@13e4b0b2-d838-11dd-a412-a1792178251d>
2008-01-12 22:03:22 +0000

    TSM-154 - wiki markup - ~~~~ shows current time, not time of revision * mostly fixed * still need to support [[User:frank]] links

--- trunk/tsm/src/main/java/com/tech4d/tsm/web/wikiText/TsmWikiModel.java
+++ trunk/tsm/src/main/java/com/tech4d/tsm/web/wikiText/TsmWikiModel.java
@@ -2,15 +2,7 @@ package com.tech4d.tsm.web.wikiText;
 
 import info.bliki.wiki.model.WikiModel;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import org.apache.commons.lang.StringUtils;
-
-import com.tech4d.tsm.auth.AuthHelper;
-
 public class TsmWikiModel extends WikiModel {
-	private static final String USER_TAGS = "~~~~";
 	private String basePath; //full base url plus port number
 	
 	public TsmWikiModel(String basePath, String imageBaseURL, String linkBaseURL) {
@@ -22,16 +14,7 @@ public class TsmWikiModel extends WikiModel {
 	public String render(String rawWikiText) {
 		// TODO Auto-generated method stub
 		String rendered = super.render(rawWikiText);
-		//implement ~~~ substitution for username
-		String username = AuthHelper.getUsername();
-		SimpleDateFormat sdf = new SimpleDateFormat("MMM dd, yyyy hh:mm a z");
-		StringBuffer replaceWith = new StringBuffer()
-			.append("<a href='")
-			.append(this.basePath)
-			.append("member/contributions.htm?user=")
-			.append(username).append("'>").append(username).append("</a> ")
-			.append(sdf.format(new Date()));
-		rendered = StringUtils.replace(rendered, USER_TAGS, replaceWith.toString());
+		rendered = SubstitutionMacro.postSignature(basePath, rendered);
 		return rendered;
 	}
 	
