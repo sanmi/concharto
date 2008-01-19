74aeac0aee111cf6883f861af05f9988af688d10
frank <frank@13e4b0b2-d838-11dd-a412-a1792178251d>
2008-01-19 19:15:40 +0000

    TSM-77 - fixed problem with spotlight list

--- tsm/src/main/java/com/tech4d/tsm/web/spotlight/SpotlightListController.java
+++ tsm/src/main/java/com/tech4d/tsm/web/spotlight/SpotlightListController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
