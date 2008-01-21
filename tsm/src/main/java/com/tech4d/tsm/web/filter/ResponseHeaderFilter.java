f913843f6cc313d5a18e2e2b75cb7c34537aee20
frank <frank@13e4b0b2-d838-11dd-a412-a1792178251d>
2008-01-21 12:49:58 +0000

    TSM-185 - javascript in cacheable files, * adding cache control http headers

---
+++ tsm/src/main/java/com/tech4d/tsm/web/filter/ResponseHeaderFilter.java
@@ -0,0 +1,43 @@
+package com.tech4d.tsm.web.filter;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * For setting http headers for various files.  NOTE: if this gets
+ * much more complicated, consider using Spring Framework filter 
+ * classes to configure instead of the limited web.xml features
+ *
+ */
+public class ResponseHeaderFilter implements Filter {
+	FilterConfig fc;
+
+	public void doFilter(ServletRequest req, ServletResponse res,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletResponse response = (HttpServletResponse) res;
+		// set the provided HTTP response parameters
+		for (Enumeration<?> e = fc.getInitParameterNames(); e.hasMoreElements();) {
+			String headerName = (String) e.nextElement();
+			response.addHeader(headerName, fc.getInitParameter(headerName));
+		}
+		// pass the request/response on
+		chain.doFilter(req, response);
+	}
+
+	public void init(FilterConfig filterConfig) {
+		this.fc = filterConfig;
+	}
+
+	public void destroy() {
+		this.fc = null;
+	}
+
+}
