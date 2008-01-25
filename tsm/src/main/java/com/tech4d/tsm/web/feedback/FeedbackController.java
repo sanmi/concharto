371b0a04e12c120ab2a4845337e4ecdbbe56eb3b
frank <frank@13e4b0b2-d838-11dd-a412-a1792178251d>
2008-01-25 17:04:16 +0000

    TSM-186 -  Show signup and remove beta program authentication * fixed jsessionid problem with home.htm

--- tsm/src/main/java/com/tech4d/tsm/web/feedback/FeedbackController.java
+++ tsm/src/main/java/com/tech4d/tsm/web/feedback/FeedbackController.java
@@ -10,7 +10,6 @@ import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.validation.BindException;
