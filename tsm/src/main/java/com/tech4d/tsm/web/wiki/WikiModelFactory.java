88e047790513abebc9735b0a91f75f67610217c7
frank <frank@13e4b0b2-d838-11dd-a412-a1792178251d>
2008-02-28 23:37:05 +0000

    TSM-232 - Add User Page and User Talk Page * major check in Remaining tasks: * change link styles * refactor to not use auditable - instead just store revisions...?

---
+++ tsm/src/main/java/com/tech4d/tsm/web/wiki/WikiModelFactory.java
@@ -0,0 +1,20 @@
+package com.tech4d.tsm.web.wiki;
+
+import info.bliki.wiki.model.WikiModel;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.tech4d.tsm.web.util.UrlFormat;
+
+
+public class WikiModelFactory {
+
+	public static WikiModel newWikiModel(HttpServletRequest request) {
+		String basePath = UrlFormat.getBasepath(request);
+		
+		return new TsmWikiModel(basePath, basePath + "images/${image}", basePath + "search/eventsearch.htm?_id=${title}");
+	}
+
+	
+
+}
