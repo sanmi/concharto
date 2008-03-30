39c6bf1753a981d49c3a86ca04e109d9430e4923
frank <frank@13e4b0b2-d838-11dd-a412-a1792178251d>
2008-03-30 00:23:43 +0000

    TSM-249 - wiki parser allows HTML = XSS * removed all HTML rendering

--- trunk/tsm/src/main/java/com/tech4d/tsm/web/wiki/TsmWikiModel.java
+++ trunk/tsm/src/main/java/com/tech4d/tsm/web/wiki/TsmWikiModel.java
@@ -1,12 +1,18 @@
 package com.tech4d.tsm.web.wiki;
 
+import info.bliki.wiki.model.Configuration;
 import info.bliki.wiki.model.WikiModel;
 
 public class TsmWikiModel extends WikiModel {
 	private String basePath; //full base url plus port number
+	private static final Configuration config = Configuration.DEFAULT_CONFIGURATION;
+	static {
+		
+		config.getTokenMap().clear();
+	}
 	
 	public TsmWikiModel(String basePath, String imageBaseURL, String linkBaseURL) {
-		super(imageBaseURL, linkBaseURL);
+		super(config, imageBaseURL, linkBaseURL);
 		this.basePath = basePath;
 	}
 
