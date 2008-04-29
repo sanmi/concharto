3da4b50d9dc74d0dcac8ebc4b668b26fa2fbe7a5
frank <frank@13e4b0b2-d838-11dd-a412-a1792178251d>
2008-04-29 23:33:35 +0000

    TSM-271 - CSS and javascript are not being chached by the browser

--- tsm/src/main/java/com/tech4d/tsm/web/filter/ResponseHeaderFilter.java
+++ tsm/src/main/java/com/tech4d/tsm/web/filter/ResponseHeaderFilter.java
@@ -1,7 +1,10 @@
 package com.tech4d.tsm.web.filter;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Enumeration;
+import java.util.TimeZone;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -11,6 +14,10 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * For setting http headers for various files.  NOTE: if this gets
  * much more complicated, consider using Spring Framework filter 
@@ -18,7 +25,11 @@ import javax.servlet.http.HttpServletResponse;
  *
  */
 public class ResponseHeaderFilter implements Filter {
+	private static final String HEADER_EXPIRES = "Expires";
+	private static final String HEADER_DATE_FORMAT = "EEE, d MMM yyyy HH:mm:ss z";
+	private static final String HEADER_CACHE_CONTROL = "Cache-Control";
 	private FilterConfig fc;
+    private Log log = LogFactory.getLog(ResponseHeaderFilter.class);
 
 	public void doFilter(ServletRequest req, ServletResponse res,
 			FilterChain chain) throws IOException, ServletException {
@@ -26,10 +37,32 @@ public class ResponseHeaderFilter implements Filter {
 		// set the provided HTTP response parameters
 		for (Enumeration<?> e = fc.getInitParameterNames(); e.hasMoreElements();) {
 			String headerName = (String) e.nextElement();
-			response.addHeader(headerName, fc.getInitParameter(headerName));
+			String value = fc.getInitParameter(headerName);
+			response.addHeader(headerName, value);
+			//special case for Cache-Control
+			if (HEADER_CACHE_CONTROL.equals(headerName)) {
+				String maxAgeStr = StringUtils.substringBetween(value, "=", ",");
+				addExpiresHeader(response, maxAgeStr);
+			}
 		}
 		// pass the request/response on
 		chain.doFilter(req, response);
+	}
+
+	private void addExpiresHeader(HttpServletResponse response, String maxAgeStr) {
+		try {
+			Integer maxAge = new Integer(maxAgeStr);
+			Calendar cal = Calendar.getInstance();
+			cal.add(Calendar.MILLISECOND, maxAge);
+			SimpleDateFormat sdf = new SimpleDateFormat(HEADER_DATE_FORMAT);
+			sdf.setTimeZone (TimeZone.getTimeZone("GMT"));
+			String expires = sdf.format(cal.getTime());
+			response.addHeader(HEADER_EXPIRES, expires);
+			         
+		} catch (NumberFormatException e) {
+			log.error("incorrectly specified web.xml - can't find maxAge", e);
+		}
+		
 	}
 
 	public void init(FilterConfig filterConfig) {
