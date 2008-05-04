d4b9307ea4a54360ec1ff44b7c7a08f6e3133a9d
frank <frank@13e4b0b2-d838-11dd-a412-a1792178251d>
2008-05-04 02:35:43 +0000

    TSM-284 - IE - js error on tags for Qu'ran

--- tsm/src/main/webapp/javascript/eventsearch.js
+++ tsm/src/main/webapp/javascript/eventsearch.js
@@ -129,31 +129,6 @@
 		return urlbase + querystring;
 	}
 	
-	// Copied from http://cass-hacks.com/articles/code/js_url_encode_decode/
-	function URLEncode (clearString) {
-	  var output = '';
-	  var x = 0;
-	  clearString = clearString.toString();
-	  var regex = /(^[a-zA-Z0-9_.]*)/;
-	  while (x < clearString.length) {
-	    var match = regex.exec(clearString.substr(x));
-	    if (match != null && match.length > 1 && match[1] != '') {
-	    	output += match[1];
-	      x += match[1].length;
-	    } else {
-	      if (clearString[x] == ' ')
-	        output += '+';
-	      else {
-	        var charCode = clearString.charCodeAt(x);
-	        var hexVal = charCode.toString(16);
-	        output += '%' + ( hexVal.length < 2 ? '0' : '' ) + hexVal.toUpperCase();
-	      }
-	      x++;
-	    }
-	  }
-	  return output;
-	}
-	
 	function hasChangedMap() {
 		return 	(_initialZoom != map.getZoom()) ||
 			(_initialCenter.lat() != map.getCenter().lat()) ||
