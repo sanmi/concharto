3059c9236e6a07d6d73cdebf76d118e49112f2ea
frank <frank@13e4b0b2-d838-11dd-a412-a1792178251d>
2008-06-16 12:00:25 +0000

    TSM-302 - support for multiple catalogs (cruising, boating, hiking, fishing, hunting, fiction, personal) * core infrastructure changes - no visible ui changes yet TSM-304 - Flag - new disposition and action to remove flags that are spam TSM-210 - Audit history - two records with revision "2" TSM-107 - Flag an event should create an audit trail

---
+++ trunk/tsm/src/main/java/com/tech4d/tsm/web/util/NoCatalogException.java
@@ -0,0 +1,11 @@
+package com.tech4d.tsm.web.util;
+
+public class NoCatalogException extends RuntimeException {
+
+	private static final long serialVersionUID = 4118355997515218282L;
+
+	public NoCatalogException(String message) {
+		super(message);
+	}
+	
+}
